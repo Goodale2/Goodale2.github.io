47757e9d89a66b526c7054d20295782c5d084019
Tanner Goodale <goodale.tanner@gmail.com>
2023-02-08 20:32:44 -0600

    Updates

---
+++ precache-manifest.df31f8a712dffee170a08a08b48e8536.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc71b40c9918b1278ea0",
+    "url": "/Goodale2/general/static/css/main.cf560c26.chunk.css"
+  },
+  {
+    "revision": "fc71b40c9918b1278ea0",
+    "url": "/Goodale2/general/static/js/main.fc71b40c.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/Goodale2/general/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "d57266edd2e5f98d4f15",
+    "url": "/Goodale2/general/static/js/runtime~main.d57266ed.js"
+  },
+  {
+    "revision": "debe786c4636a2a21abc8380f5a7f795",
+    "url": "/Goodale2/general/index.html"
+  }
+];
